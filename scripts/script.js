0c33f0e712084fa18ed709179487445730ca9e4e
angelalita77 <anjeanette@gmail.com>
2025-08-18 23:22:18 -0400

    to-do list can add a task. Need to fix so textbox is empty after adding a task

---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+// cache DOM elements 
+const taskInput = document.getElementById("enterTask"); // Task entered by user
+const addTaskBtn = document.getElementById("submitTask"); // The "Add Task" button
+const taskList = document.getElementById("taskList"); //The list will be created
+
+addTaskBtn.addEventListener('click', addTask)
+
+
+//Function to retrieve task entered by user and then place task
+// in list
+function addTask(e) {
+    e.preventDefault();
+    
+    // save user input into task
+    const task = taskInput.value;
+
+    // Checks if there is anything entered in text box
+   if(task) {
+    createTaskElement(task);
+    
+   } else { 
+        // alert that nothing has been entered
+     taskInput.value = alert('You need to enter a task.')
+   }
+
+}
+
+function createTaskElement(task) {
+
+    // create the <li></li> tags
+    const listItem = document.createElement('li')
+    //console.log(listItem)
+    listItem.textContent = task;  // save the contents of task to listItem
+    // add <li> element with task as a child to the <ul> list element
+    taskList.appendChild(listItem);
+    
+}
