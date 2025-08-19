f63b7ed4662fe76d08aa21e6fbf27a8dbf5b94ad
angelalita77 <anjeanette@gmail.com>
2025-08-18 23:57:44 -0400

    Added an Urgent button to add a task in a red item list background

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,10 @@
 const taskInput = document.getElementById("enterTask"); // Task entered by user
 const addTaskBtn = document.getElementById("submitTask"); // The "Add Task" button
 const taskList = document.getElementById("taskList"); //The list will be created
+const urgentTask = document.getElementById("submitUrgent"); //The list will be created
 
-addTaskBtn.addEventListener('click', addTask)
+addTaskBtn.addEventListener('click', addTask);
+urgentTask.addEventListener('click', addUrgent);
 
 
 //Function to retrieve task entered by user and then place task
@@ -27,6 +29,26 @@ function addTask(e) {
 
 }
 
+function addUrgent(e) {
+    e.preventDefault();
+    
+    // save user input into task
+    const task = taskInput.value;
+
+    // Checks if there is anything entered in text box
+   if(task) {
+    createUrgentTaskElement(task);
+    taskInput.value = ""; //empty textbox after adding task
+    taskInput.focus(); //keey cursor active in textbox after adding task
+    
+   } else { 
+        // alert that nothing has been entered
+     taskInput.value = alert('You need to enter a task.')
+   }
+
+}
+
+
 function createTaskElement(task) {
 
     // create the <li></li> tags
@@ -37,3 +59,16 @@ function createTaskElement(task) {
     taskList.appendChild(listItem);
     
 }
+
+function createUrgentTaskElement(task) {
+
+    // create the <li></li> tags
+    const listItem = document.createElement('li')
+    //console.log(listItem)
+    listItem.textContent = task;  // save the contents of task to listItem
+    // add <li> element with task as a child to the <ul> list element
+    taskList.appendChild(listItem);
+    document.querySelector('li').style.backgroundColor = "#ffc2c2";
+    
+}
+
